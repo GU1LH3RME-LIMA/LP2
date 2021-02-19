6277e16fac7f5575cbd5926533969d84dc882b43
Guilherme de Lima <gmendoncalima@hotmail.com>
2021-02-18 21:00:13 -0300

    1

---
+++ 01-Hello/HelloWorldApp.java
@@ -0,0 +1,7 @@
+// arquivo HeloWorldApp.java feita por Guilherme de Lima
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
