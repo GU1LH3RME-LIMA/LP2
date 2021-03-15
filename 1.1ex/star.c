b3a4da04a0097bdeb0165e277455e3f45f4d2bc3
Guilherme de Lima <gmendoncalima@hotmail.com>
2021-03-15 08:24:04 -0300

    alterado

--- 1.1ex/star.c
+++ 1.1ex/star.c
@@ -3,15 +3,16 @@
 typedef struct Estrela{
     int cantos;
     double raio,altura;
+    int x,y;
 }Estrela;
 
 void print(Estrela *);
 
 void main(void){
-    Estrela x={6,0.5,71.8};
+    Estrela x={6,0.5,71.8,3,4};
     print(&x);
 }
 
 void print(Estrela *x){
-    printf("Uma estrela com %i cantos,raio de %.2lf e  %.2lf mm de altura \n",x->cantos,x->raio,x->altura);
+    printf("Uma estrela com %i cantos,raio de %.2lf e  %.2lf mm de altura na posição (%i,%i) \n",x->cantos,x->raio,x->altura,x->x,x->y);
 }
