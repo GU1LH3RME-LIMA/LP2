f61951408fb85a81a70d625d3f5da945916ffeb2
Guilherme de Lima <gmendoncalima@hotmail.com>
2021-05-16 15:22:53 -0300

    mudanca 2

--- Projeto/button/Button.java
+++ Projeto/button/Button.java
@@ -20,8 +20,10 @@ public class Button implements IVisible {
         this.fig.h = DIM-PAD*2;
     }
 
-    public boolean clicked (int x, int y) {
-        return SPC<=x && x<=SPC+DIM && SPC+this.idx*DIM<=y && y<=SPC+this.idx*DIM+DIM;
+    public boolean clicked (MouseEvent evt) {
+        if (this.fig.contains(evt.getPoint()))
+				return true;
+			return false;
     }
 
     public void paint (Graphics g, boolean focused) {
