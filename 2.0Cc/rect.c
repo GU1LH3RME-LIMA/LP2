ee57793086bf8f53fa3f813d925196a597234a8d
Guilherme de Lima <gmendoncalima@hotmail.com>
2021-05-24 19:05:35 -0300

    tipo abstrato

---
+++ 2.0Cc/rect.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rect.h"
+typedef struct {
+    int r,g,b;
+} Color;
+typedef struct Rect{
+    int x,y,h,w;
+    Color cc,cf;
+}Rect;
+Rect* rect_new () {
+    Rect*   this  = malloc(sizeof(Rect));
+    this->x = rand()%100;
+    this->y = rand()%100;
+    this->w = rand()%30;
+    this->h = rand()%30;
+}
+
+void rect_print (Rect* this) {
+    printf("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+           this->w, this->h, this->x, this->y);
+}
+
+void rect_drag(Rect* this, int x,int y){
+    this->x+=x;
+    this->y+=y;
+}
---
+++ 2.0Cc/rect.h
@@ -0,0 +1,9 @@
+#ifndef RECT_H
+#define RECT_H
+
+typedef struct Rect Rect;
+Rect* rect_new (void);
+void rect_print (Rect*);
+void rect_drag (Rect*, int,int);
+
+#endif
